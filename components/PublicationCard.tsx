50276d4bc19b108500fd06a46163c6a47584bdf4
Morten Hagenau <moha@frb-forsyning.dk>
2022-11-28 14:33:17 +0100

    Fixed links

--- components/PublicationCard.tsx
+++ components/PublicationCard.tsx
@@ -14,7 +14,7 @@ function PublicationCard({ imageURL, publicationTitle, publicationDesc, currPubl
   return (
     <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5
                     items-center justify-center p-20 md:p-44 h-screen'> 
-        <a href={link} target={"_blank"}>
+        <a href={link} target={"_blank"} rel="noreferrer">
             <motion.img
                 initial={{ y: -200 }}
                 transition={{ duration: 1.2 }}
@@ -33,7 +33,7 @@ function PublicationCard({ imageURL, publicationTitle, publicationDesc, currPubl
                 </span>
             </h4>
             <p className='text-3xl font-semibold text-center'>
-            <a href={link} target={"_blank"}> {publicationTitle} </a>
+            <a href={link} target={"_blank"} rel="noreferrer"> {publicationTitle} </a>
             </p>
             <p className='text-lg text-center md:text-left'>
                 {publicationDesc}
